1cc07dc9071712ba5c56a9c87b5db504cd298e07
Bryan Gutierrez <soybryanguti98@gmail.com>
2022-07-23 16:00:29 -0400

    Manupulacion del DOM terminada

--- web/piedra_papel_tijera/js/main.js
+++ web/piedra_papel_tijera/js/main.js
@@ -1,34 +1,77 @@
 function newGame(select){
     let playerSelection = select.srcElement.value
+    let playerEmoji = ""
+    let playerScore = parseInt(document.getElementById("player-score").innerHTML)
+
     let botSelection = chooseRobotOption()
+    let botEmoji = ""
+    let botScore = parseInt(document.getElementById("bot-score").innerHTML)
+
+    if(playerSelection === "rock"){
+        playerEmoji = "✊"
+    }
+    if(playerSelection === "scissor"){
+        playerEmoji = "✌"
+    }
+    if(playerSelection === "paper"){
+        playerEmoji = "✋"
+    }
+    if(botSelection === "rock"){
+        botEmoji = "✊"
+    }
+    if(botSelection === "scissor"){
+        botEmoji = "✌"
+    }
+    if(botSelection === "paper"){
+        botEmoji = "✋"
+    }
 
     if(playerSelection === botSelection){
-        console.log("DRAW")
+        document.getElementById("player-selection").innerHTML = playerEmoji
+        document.getElementById("bot-selection").innerHTML = botEmoji
+        document.getElementById("message").innerHTML = "DRAW"
     }
-    else if(playerSelection === "rock" && botSelection === "scissor"){
-        console.log("YOU WIN")
+    if(playerSelection === "rock" && botSelection === "scissor"){
+        document.getElementById("player-selection").innerHTML = playerEmoji
+        document.getElementById("bot-selection").innerHTML = botEmoji
+        document.getElementById("message").innerHTML = "WIN"
+        document.getElementById("player-score").innerHTML = (playerScore + 1).toString()
     }
-    else if(playerSelection === "scissor" && botSelection === "paper"){
-        console.log("YOU WIN")
+    if(playerSelection === "scissor" && botSelection === "paper"){
+        document.getElementById("player-selection").innerHTML = playerEmoji
+        document.getElementById("bot-selection").innerHTML = botEmoji
+        document.getElementById("message").innerHTML = "WIN"
+        document.getElementById("player-score").innerHTML = (playerScore + 1).toString()
     }
-    else if(playerSelection === "paper" && botSelection === "rock"){
-        console.log("YOU WIN")
+    if(playerSelection === "paper" && botSelection === "rock"){
+        document.getElementById("player-selection").innerHTML = playerEmoji
+        document.getElementById("bot-selection").innerHTML = botEmoji
+        document.getElementById("message").innerHTML = "WIN"
+        document.getElementById("player-score").innerHTML = (playerScore + 1).toString()
     }
-    else if(playerSelection === "rock" && botSelection === "paper"){
-        console.log("YOU LOSE")
+    if(playerSelection === "rock" && botSelection === "paper"){
+        document.getElementById("player-selection").innerHTML = playerEmoji
+        document.getElementById("bot-selection").innerHTML = botEmoji
+        document.getElementById("message").innerHTML = "LOST"
+        document.getElementById("bot-score").innerHTML = (botScore + 1).toString()
     }
-    else if(playerSelection === "scissor" && botSelection === "rock"){
-        console.log("YOU LOSE")
+    if(playerSelection === "scissor" && botSelection === "rock"){
+        document.getElementById("player-selection").innerHTML = playerEmoji
+        document.getElementById("bot-selection").innerHTML = botEmoji
+        document.getElementById("message").innerHTML = "LOST"
+        document.getElementById("bot-score").innerHTML = (botScore + 1).toString()
     }
-    else if(playerSelection === "paper" && botSelection === "scissor"){
-        console.log("YOU LOSE")
+    if(playerSelection === "paper" && botSelection === "scissor"){
+        document.getElementById("player-selection").innerHTML = playerEmoji
+        document.getElementById("bot-selection").innerHTML = botEmoji
+        document.getElementById("message").innerHTML = "LOST"
+        document.getElementById("bot-score").innerHTML = (botScore + 1).toString()
     }
 }
 
 function chooseRobotOption(){
     let options = ["rock", "scissor", "paper"]
     let choosenRobotOption = options[Math.floor((Math.random() * 3))]
-
     return choosenRobotOption
 }
 
@@ -39,7 +82,6 @@ function gameStart(){
     scissorButton.addEventListener('click', newGame)
     let paperButton = document.getElementById("paper-button")
     paperButton.addEventListener('click', newGame)
-
 }
 
 window.addEventListener('load', gameStart)
